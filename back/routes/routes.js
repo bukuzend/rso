2cebaa116c1947c1547f91d0d646fae655c26f07
bukuzend <bukuzend@gmail.com>
2024-10-19 10:07:26 +0300

    lab2

--- back/routes/routes.js
+++ back/routes/routes.js
@@ -1,6 +1,43 @@
-module.exports = function(app, db) {  
-    app.post('/notes', (req, res) => {    
-        console.log(req.body)       
-        res.send('Hello')  }); 
-    };
+module.exports = function (app, client) {
+
+    app.post('/notes/add', async (req, res) => {
+            const note = {
+                body: req.body.body,
+                title: req.body.title
+            };
+
+            console.log(req.body);
+            const url = "mongodb+srv://PopovIO:PopovIO@popovio.lbnuy.mongodb.net/?retryWrites=true&w=majority&appName=PopovIO";
+
+            await client.connect();
+
+            await client.db("PopovIO").collection('notes').insertOne(note);
+            res.send(note)
+        });
+
+    app.get('/notes/all', async (req, res) => {
+        const url = "mongodb+srv://PopovIO:PopovIO@popovio.lbnuy.mongodb.net/?retryWrites=true&w=majority&appName=PopovIO";
+
+        await client.connect();
+        const note = await client.db("PopovIO").collection('notes').find({}).toArray();
+
+        res.send(note)
+        });
+
+    app.patch('/notes/update', async (req, res) => {
+        const url = "mongodb+srv://PopovIO:PopovIO@popovio.lbnuy.mongodb.net/?retryWrites=true&w=majority&appName=PopovIO";
     
+        await client.connect();
+        const note = await client.db("PopovIO").collection('notes').updateOne({title: req.query.title}, { $set: {body: req.body.newText}})
+        res.send(note)
+    })
+    app.delete('/notes/delete', async (req,res) => {
+        const url = "mongodb+srv://PopovIO:PopovIO@popovio.lbnuy.mongodb.net/?retryWrites=true&w=majority&appName=PopovIO";
+    
+        await client.connect();
+        const note = await client.db("PopovIO").collection('notes').deleteOne({title: req.query.title})
+        res.send(note)
+    })
+    
+    }
+ 
--- back/server.js
+++ back/server.js
@@ -5,11 +5,38 @@ const app            = express();
 const port = 8000; 
 
 app.use(bodyParser.urlencoded({extended:true}))
-
-require('./routes/index')(app, {}); 
-
-
-app.listen(port, () => {  
-console.log('We are live on ' + port);
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
+
+
+const url = "mongodb+srv://PopovIO:PopovIO@popovio.lbnuy.mongodb.net/?retryWrites=true&w=majority&appName=PopovIO";
+
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'PopovIO';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+
+  return client;
+}
+
+main()
+  .then(async (client) => {
+    require('./routes/index')(app, client);
+
+    app.listen(port, () => {
+      console.log('We are live on ' + port);
+    });
+  })
+  .catch(console.error)
+  .finally(() => client.close());
+
 
